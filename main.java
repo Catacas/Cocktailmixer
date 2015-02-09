413f43168a49da3e3a18ca0605ec413e21423a90
Fagamir <ole.evers@ewetel.net>
2015-02-09 12:18:55 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,22 @@
+package Touchdisplay;
+
+public class Main {
+	public static void main (String[] args) {
+		
+		GUI Fenster = new GUI();
+		Fenster.mitoderohne();
+		
+		//Bekanntmachung bkm = new Bekanntmachung();
+		//bkm.Hallo();
+		//Hauptmenue haupt = new Hauptmenue();
+		//haupt.haupmenue_cocktails();
+		
+		
+	}
+	
+
+
+	}
+
+	
+	
